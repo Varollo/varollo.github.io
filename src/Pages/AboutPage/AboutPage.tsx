de605a0498c7aeda0c601f7092b9bab6e37fc012
Gabriel Carollo <rimoldi98@gmail.com>
2026-01-05 10:15:02 -0300

    Fixed css in about page when screen is too small

--- src/Pages/AboutPage/AboutPage.tsx
+++ src/Pages/AboutPage/AboutPage.tsx
@@ -16,51 +16,55 @@ import "./AboutPage.css";
 const AboutPage = () => {
   return (
     <Window title="ABOUT ME">
-      <div className="profileTop">
-        <ProfileImg imgUrl={ProfileImgUrl} />
-        <ul className="profileBox profileInfo">
-          {aboutPageData.info.map((info, i) => {
-            const [[key, value]] = Object.entries(info);
-            return <ProfileItem key={`${key}_${i}`} title={key} content={value} />;
-          })}
-        </ul>
-      </div>
-
-      <div className="profileBottom">
-        <div className="profileBox profileSocials">
-          <ul>
-            <Link to="https://varollo.github.io">
-              <ProfileItem
-                title={<img src={WebIcon} alt="" />}
-                content="varollo.github.io"
-              />
-            </Link>
-
-            <Link to="https://github.com/varollo">
-              <ProfileItem
-                title={<img src={GithubLogo} alt="" />}
-                content="github.com/varollo"
-              />
-            </Link>
-
-            <Link to="https://www.linkedin.com/in/gabriel-carollo-3699181a0/">
-              <ProfileItem
-                title={<img src={LinkedinLogo} alt="" />}
-                content="in/gabriel-carollo"
-              />
-            </Link>
-
-            <Link to="https://bsky.app/profile/varollo.bsky.social">
-              <ProfileItem
-                title={<img src={BlueSkyLogo} alt="" />}
-                content="varollo.bsky.social"
-              />
-            </Link>
+      <div className="profileWindow">
+        <div className="profileTop">
+          <ProfileImg imgUrl={ProfileImgUrl} />
+          <ul className="profileBox profileInfo">
+            {aboutPageData.info.map((info, i) => {
+              const [[key, value]] = Object.entries(info);
+              return (
+                <ProfileItem key={`${key}_${i}`} title={key} content={value} />
+              );
+            })}
           </ul>
         </div>
-        <div className="profileBox profileBio">
-          <h1>Who am I?</h1>
-          <p>{aboutPageData.bio}</p>
+
+        <div className="profileBottom">
+          <div className="profileBox profileSocials">
+            <ul>
+              <Link to="https://varollo.github.io">
+                <ProfileItem
+                  title={<img src={WebIcon} alt="" />}
+                  content="varollo.github.io"
+                />
+              </Link>
+
+              <Link to="https://github.com/varollo">
+                <ProfileItem
+                  title={<img src={GithubLogo} alt="" />}
+                  content="github.com/varollo"
+                />
+              </Link>
+
+              <Link to="https://www.linkedin.com/in/gabriel-carollo-3699181a0/">
+                <ProfileItem
+                  title={<img src={LinkedinLogo} alt="" />}
+                  content="in/gabriel-carollo"
+                />
+              </Link>
+
+              <Link to="https://bsky.app/profile/varollo.bsky.social">
+                <ProfileItem
+                  title={<img src={BlueSkyLogo} alt="" />}
+                  content="varollo.bsky.social"
+                />
+              </Link>
+            </ul>
+          </div>
+          <div className="profileBox profileBio">
+            <h1>Who am I?</h1>
+            <p>{aboutPageData.bio}</p>
+          </div>
         </div>
       </div>
     </Window>
